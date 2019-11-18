9eaaf8ee4c557caccb711acbf421c75a6b9b2c47
themangomago <markus.schneider84@gmail.com>
2019-11-18 23:46:47 +0100

    TextDebug

--- src/Levels/Level.gd
+++ src/Levels/Level.gd
@@ -5,17 +5,44 @@ class_name Level
 onready var playerScene = preload("res://src/Player/Player.tscn")
 var player = null
 
+var debugCat
+
 func _ready():
 	print("level ready")
 	var gm = Global.getGameManager()
 	gm.setWorlds($WorldDigable, $WorldNormal)
 	
 	spawnPlayer()
+	DebugSetup()
+	
+
+func DebugSetup():
+	if Global.debug:
+		debugCat = Debug.addCategory("SpawnAt")
+		print("level")
+		for spawn in get_tree().get_nodes_in_group("spawn"):
+			#addOption(category, optionName, callback, parameter):
+			Debug.addOption(debugCat, str(spawn.name + " (" + spawn.spawnName  + ")" ), funcref(self, "DebugPortPlayer"), str(spawn.name))
+
+func DebugPortPlayer(to):
+	assert(player != null)
+	for spawn in get_tree().get_nodes_in_group("spawn"):
+		if spawn.name == to:
+			print(spawn.position)
+			player.position = spawn.position
+			player.restartPoint = spawn.position
+			var pos = (player.position / Vector2(480, 272))
+			if pos.y < 0: pos.y -= 1
+			if pos.x < 0: pos.x -= 1
+			pos = Vector2(int(pos.x), int(pos.y))
+			Global.getCam().transitionToScreen(pos)
+			break
+			
 
 func spawnPlayer(at = "START"):
 	print("spawn player")
 	for spawn in get_tree().get_nodes_in_group("spawn"):
-		if spawn.spawnName == at:
+		if spawn.spawnName == at: #TODO: maybe rely on real spawn name or make a tick for start node
 			player = playerScene.instance()
 			player.position = spawn.position
 			$Objects.add_child(player)
--- src/_Autoloads/Debug.gd
+++ src/_Autoloads/Debug.gd
@@ -1,13 +1,10 @@
 extends Node
 
 var menuOpen = false
-var options = []
+var subMenu = 0 
 
-func _ready():
-#	addOption("Variant 1", funcref(self, "test"), true)
-#	addOption("Variant 2", funcref(self, "test"), 15)
-#	addOption("Variant 3", funcref(self, "test"), "string")
-	pass
+var data = []
+var options = []
 
 func _input(event):
 	if Global.debug:
@@ -19,7 +16,7 @@ func _input(event):
 				#Open Debug Menu
 				elif event.scancode == KEY_F4:
 					_displayMenu()
-					print(">")
+					subMenu = 0
 					menuOpen = true
 				elif menuOpen:
 					_handleOptions(event.scancode)
@@ -27,22 +24,57 @@ func _input(event):
 func _displayMenu():
 	print(" ")
 	print("Menu============")
-	for points in options:
+	for points in data:
 		print(str(points.index) + "." + points.name)
 	print("================")
+
+func _displaySubMenu(index):
+	print(" ")
+	print("SubMenu---------")
+	for point in data[subMenu - 1].options:
+		print(str(point.index) + "." + point.name)
+	print("----------------")
 
 func _handleOptions(code):
 	if code >= KEY_0 and code <= KEY_9:
 		var index = code - KEY_0
-		for point in options:
-			if point.index == index:
-				point.callback.call_func(point.parameter)
-				menuOpen = false
-				break
+		print(">"+str(index))
+		if subMenu == 0:
+			for category in data:
+				if category.index == index:
+					subMenu = index
+					_displaySubMenu(index)
+		else:
+			for point in data[subMenu - 1].options:
+				if point.index == index:
+					point.callback.call_func(point.parameter)
+					menuOpen = false
+					break
 
-func addOption(optionName, callback, parameter):
-	var index = options.size() + 1
-	options.append({"index": index, "name": optionName, "callback": callback, "parameter": parameter})
+func _ready():
+#	var cat1 = addCategory("Test 1")
+#	addOption(cat1, "Variant 1", funcref(self, "test"), true)
+#	addOption(cat1, "Variant 2", funcref(self, "test"), false)
+#	var cat2 = addCategory("Test 2")
+#	addOption(cat2, "Variant 3", funcref(self, "test"), true)
+#	addOption(cat2, "Variant 4", funcref(self, "test"), false)
+	pass
+
+func addCategory(categoryName):
+	for i in range(data.size()):
+		if data[i].name == categoryName:
+			return i
+			
+	var index = data.size() + 1 
+	data.append({"index": index, "name": categoryName, "options": []})
+	return index 
+
+func addOption(category, optionName, callback, parameter):
+	var index = data[category - 1].options.size() + 1
+	data[category - 1].options.append({"index": index, "name": optionName, "callback": callback, "parameter": parameter})
+
+func clearOptions(category):
+	data[category - 1].options.clear()
 
 func test(value):
 	print("-> Selected Value: " + str(value) )
